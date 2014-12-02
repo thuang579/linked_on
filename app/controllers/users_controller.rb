1d451c9b0fda6182ffa80f18077e7876f832e499
thuang <thuang579@gmail.com>
2014-12-01 22:07:35 -0800

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			redirect_to root_path
+		else
+			flash[:notice] = @user.errors.full_sentences.to_sentence
+			render :new
+		end
+
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :organization_id)
+	end
+
+
+end
